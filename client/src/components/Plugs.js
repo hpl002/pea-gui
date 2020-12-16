22c337fa5b5fb2fe6b6dbceb78ad9b6c030d9be9
Herman Stoud Platou <hsplatou@gmail.com>
2020-12-16 14:14:37 +0100

    react stepper wizard init

---
+++ client/src/components/Plugs.js
@@ -0,0 +1,20 @@
+/* eslint-disable react/prop-types */
+import React, { Fragment } from 'react';
+import Stars from './Stars';
+
+/* 
+const Link = ({ href, children }) => (
+    // eslint-disable-next-line react/jsx-no-target-blank
+    <a href={href} className={'alert-link'} target='_blank'>
+        { children }
+    </a>
+);
+*/
+
+const Plugs = () => (
+    <Fragment>
+        <Stars />
+    </Fragment>
+);
+
+export default Plugs;

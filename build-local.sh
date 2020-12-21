71e90822114cc9b1cbf6d1d12b1ef3521d965437
Herman Stoud Platou <hsplatou@gmail.com>
2020-12-21 02:03:24 +0100

    local dev script for docker development

--- build-local.sh
+++ build-local.sh
@@ -1,35 +1,57 @@
 #!/bin/zsh
-REPO=hpl002/pea-gui/
+REPO=pea-gui
+IMAGE_NAME=gui
+TAG=latest
+REGISTRY=local/hpl002/$REPO/$IMAGE_NAME:$TAG
+# hpl002/pea-gui/gui:latest
+
 # script for local development
 # easily swaps out the existing container with a new build
 
-
-# Get image id 
+# get image id 
 IMAGE_ID=$(docker images -f label=app=pea:gui -q)  
-# Get container name
+# get container name
 CONTAINER_ID=$(docker ps -a -f ancestor=$IMAGE_ID --format='{{json .}}' | jq -r '.ID'   )
-# Get container ports
+# get container ports
 CONTAINER_PORTS=$(docker ps -a -f ancestor=$IMAGE_ID --format='{{json .}}' | jq -r '.Ports'   )
 
-# source port
 
-# target port
+GET_CONTAINER_PORT_SOURCE()
+{
+IFS="->" read -a cleaned <<< $CONTAINER_PORTS
+read -a uri <<< $cleaned
+IFS=":" read -a host_port <<< $uri
+read -a port <<< $host_port
+echo "${port[1]}"
+}
 
- 
-# Stop container
+GET_CONTAINER_PORT_TARGET()
+{
+IFS="->" read -a cleaned <<< $CONTAINER_PORTS
+read -a uri <<< $cleaned
+IFS="/" read -a split <<< ${uri[1]}
+read -a p <<< $split
+echo ${p[0]}
+}
+
+# get source port for starting up a new container
+# localhost
+CONTAINER_PORT_SOURCE=$(GET_CONTAINER_PORT_SOURCE)
+# get target port for starting up a new container
+# container exposed port
+CONTAINER_PORT_TARGET=$(GET_CONTAINER_PORT_TARGET)
+
+# stop container
 $(docker container stop $CONTAINER_ID)
-# Delete container
+# delete container
 $(docker container rm $CONTAINER_ID)
-# Delete image
+# delete image
 $(docker rmi $IMAGE_ID)
 
 # build new image with the exact same tags 
-docker build . --file Dockerfile --tag local/$REPO
-# start image on same port
+docker build . --file Dockerfile --tag $IMAGE_NAME
+# build new image with the exact same tags 
+docker tag $IMAGE_NAME $REGISTRY
 
-# docker run -p $TARGET_PORT:#CONTAINER_PORT
-
-
-# get info pertaining to 
-echo $CONTAINER_NAME
-
+# start image on same ports as previously used 
+docker run -d -p $CONTAINER_PORT_SOURCE:$CONTAINER_PORT_TARGET $REGISTRY

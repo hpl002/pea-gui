7520db3b07e75e8f3fa3f3f3bc01aa49a3ce3ac4
Herman Stoud Platou <hsplatou@gmail.com>
2021-01-31 13:35:35 +0100

    fix content alignment

--- src/components/pages/First.js
+++ src/components/pages/First.js
@@ -1,17 +1,19 @@
-import Stats from "../../helper"
-import React from 'react';
+import Stats from "../../helper";
+import React from "react";
+import styles from "../../scss/pages.module.scss";
 const First = (props) => {
   return (
-    <div>
-      <h3 className="text-center">
-        Create base model from log data or from scratch
-      </h3>
-      <div style={{ width: "30vw", margin: "auto" }}>
-        <Stats step={2} title={"Create model from log data"} {...props} />
-        <Stats step={3} title={"Create model from scratch"} {...props} />
+    <div className={`${styles.content} ${styles.center}`}>
+      <div>
+        <h3 className="text-center">
+          Create base model from log data or from scratch
+        </h3>
+        <div style={{ width: "30vw", margin: "auto" }}>
+          <Stats step={2} title={"Create model from log data"} {...props} />
+          <Stats step={3} title={"Create model from scratch"} {...props} />
+        </div>
       </div>
     </div>
   );
 };
-
 export default First;

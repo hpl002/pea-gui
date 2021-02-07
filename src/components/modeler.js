6342ec3706bb7c3454f6b08acf834455c1b92c8b
Herman Stoud Platou <hsplatou@gmail.com>
2021-02-07 23:28:51 +0100

    added properties panel

--- src/components/modeler.js
+++ src/components/modeler.js
@@ -1,16 +1,21 @@
 /* eslint-disable react/prop-types */
 import React, { useEffect, useState } from "react";
 import ReactDOM from "react-dom";
-import Modeler from "bpmn-js/lib/Modeler";
-import propertiesPanelModule from "bpmn-js-properties-panel";
-import propertiesProviderModule from "bpmn-js-properties-panel/lib/provider/bpmn";
-import "bpmn-js/dist/assets/diagram-js.css";
-import "bpmn-js/dist/assets/bpmn-font/css/bpmn-embedded.css";
 import axios from "axios";
 import Styled from "styled-components";
+
 import SyntaxHighlighter from "react-syntax-highlighter";
 import { docco } from "react-syntax-highlighter/dist/esm/styles/hljs";
-var modelerConfig = require("../configs/modeler.json");
+
+import Modeler from "bpmn-js/lib/Modeler";
+import "bpmn-js/dist/assets/diagram-js.css"; 
+import "bpmn-js/dist/assets/bpmn-font/css/bpmn-embedded.css";
+import "bpmn-js-properties-panel/dist/assets/bpmn-js-properties-panel.css"
+
+
+import propertiesPanelModule from "bpmn-js-properties-panel";
+import propertiesProviderModule from "bpmn-js-properties-panel/lib/provider/camunda";
+import camundaModdleDescriptor from "camunda-bpmn-moddle/resources/camunda";
 
 const Component = (props) => {
   if (props.display) {
@@ -41,7 +46,6 @@ function App(props) {
   const [localModel, localModelSet] = useState("");
   const [displayModeler, setDisplayModeler] = useState(true);
   const canvas = document.getElementById("canvas");
-  const properties = document.getElementById("properties");
 
   useEffect(() => {
     if (condition) {
@@ -74,13 +78,13 @@ function App(props) {
       ReactDOM.findDOMNode(canvas).childElementCount === 0
     ) {
       const bpmnModeler = new Modeler({
-        additionalModules: [propertiesPanelModule, propertiesProviderModule],
-        container: "#canvas",         
-        keyboard: {
-          bindTo: document,
+        container: "#canvas",
+        propertiesPanel: {
+          parent: "#properties",
         },
+        additionalModules: [propertiesPanelModule, propertiesProviderModule],
         moddleExtensions: {
-          qa: modelerConfig,
+          camunda: camundaModdleDescriptor,
         },
       });
       bpmnModeler
@@ -107,10 +111,11 @@ function App(props) {
   return (
     <Wrapper>
       <ContentWrapper>
-      <ModelerWraper id="modeler" >         
-        <div id="canvas"style={{ display: displayModeler ? "initial" : "none" }}/>                 
-        <Component display={!displayModeler} code={model} style={{height:"inherit"}}/>
-      </ModelerWraper>
+        <ModelerWraper id="modeler">
+          <Canvas id="canvas" style={{ display: displayModeler ? "flex" : "none" }} />
+          <Properties id="properties"/>
+          <Component display={!displayModeler} code={model} style={{ height: "inherit" }} />
+        </ModelerWraper>
       </ContentWrapper>
       <Styledbutton
         className="btn btn-info btn-block"
@@ -125,12 +130,26 @@ function App(props) {
 }
 export default App;
 
+
+
 const ModelerWraper = Styled.div`     
   border: 1px solid #000000;
   height: 100%;
   width: 100%;
   margin: auto;
   marginBottom:10px;
+  display:flex;
+`;
+
+const Properties = Styled.div`     
+background-color: #f8f8f8;
+width: 20%;
+`;
+
+const Canvas = Styled.div`     
+display: flex;
+height: inherit;
+width: 80%;
 `;
 
 const Wrapper = Styled.div`     

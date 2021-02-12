6aeeae3e8c7451a0f52aef1acdff98a4a4f8743b
Herman Stoud Platou <hsplatou@gmail.com>
2021-02-12 23:29:35 +0100

    created simple endpoint for reading the init xml file

--- server/index.js
+++ server/index.js
@@ -1,12 +1,23 @@
+const express = require("express");
+const fs = require("fs"); 
+const convert = require("xml-js");
+const app = express();
+const port = 3050;
 
-const express = require('express')
-const app = express()
-const port = 3050
-
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+// read xml file
+// parse xml to json
+// send json file
+app.get("/init", (req, res) => {
+  let data = "";
+  try {
+    data = fs.readFileSync("../static/init.bpmn", "utf8");
+    res.status(200).send(data);
+  } catch (error) {
+      console.error(error)
+    res.sendStatus(500);
+  }
+});
 
 app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`)
-})
+  console.log(`Example app listening at http://localhost:${port}`);
+});

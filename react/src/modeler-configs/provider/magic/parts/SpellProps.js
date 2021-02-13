85fa87773190ce3a7aeb4a5a523735ed504c7a9c
Herman Stoud Platou <hsplatou@gmail.com>
2021-02-13 22:53:36 +0100

    merged all tabs into one and started to add groups instead

--- react/src/modeler-configs/provider/magic/parts/SpellProps.js
+++ react/src/modeler-configs/provider/magic/parts/SpellProps.js
@@ -1,16 +1,7 @@
 import entryFactory from 'bpmn-js-properties-panel/lib/factory/EntryFactory';
-
-import {
-  is
-} from 'bpmn-js/lib/util/ModelUtil';
-
+import { is } from 'bpmn-js/lib/util/ModelUtil';
 
 export default function(group, element, translate) {
-
-  // Only return an entry, if the currently selected
-  // element is a start event.
-
-  if (is(element, 'bpmn:StartEvent')) {
     group.entries.push(entryFactory.textField(translate, {
       id : 'spell',
       description : 'Apply a black magic spell',
@@ -18,4 +9,3 @@ export default function(group, element, translate) {
       modelProperty : 'spell'
     }));
   }
-}

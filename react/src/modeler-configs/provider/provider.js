4cdfccd6dccdb0e39e549bb57e1cf8b4a72a956f
Herman Stoud Platou <hsplatou@gmail.com>
2021-03-01 10:52:04 +0100

    render tabs based on current selected element

--- react/src/modeler-configs/provider/provider.js
+++ react/src/modeler-configs/provider/provider.js
@@ -4,12 +4,15 @@ import PropertiesActivator from "bpmn-js-properties-panel/lib/PropertiesActivato
 import generalGroup from "./groups/general"
 import scenarioGroup from "./groups/scenario"
 import scenarioParametersGroup from "./groups/scenarioParameters"
+import elementParametersGroup from "./groups/elementParameters"
 
 export default function PropertiesProvider(eventBus, bpmnFactory, canvas, elementRegistry, translate, moddle, commandStack) {
 
   PropertiesActivator.call(this, eventBus);
 
   this.getTabs = function (element) {
+
+    const tabsArr = []
 
     /*
     var generalTab = {
@@ -25,21 +28,35 @@ export default function PropertiesProvider(eventBus, bpmnFactory, canvas, elemen
     };
     */
 
-    var scenarioAttributes = {
-      id: "scenarioTab",
-      label: "Scenario attributes",
-      groups: scenarioGroup({ element, bpmnFactory, canvas, elementRegistry, translate })
-    };
+    // TODO: conditionally render tabs. For example, we do not want to even show the tabs pertaining to the scenario whenever we have not selected the root process element
 
-    var scenarioParameters = {
-      id: "scenarioParameters",
-      label: "Scenario Parameters",
-      groups: scenarioParametersGroup({ element, bpmnFactory, canvas, elementRegistry, translate })
-    };
+    if (element.type === "bpmn:Process") {
+      var scenarioAttributes = {
+        id: "scenarioTab",
+        label: "Scenario attributes",
+        groups: scenarioGroup({ element, bpmnFactory, canvas, elementRegistry, translate })
+      };
+      tabsArr.push(scenarioAttributes)
 
+      var scenarioParameters = {
+        id: "scenarioParameters",
+        label: "Scenario Parameters",
+        groups: scenarioParametersGroup({ element, bpmnFactory, canvas, elementRegistry, translate })
+      };
 
+      tabsArr.push(scenarioParameters)
+    }
+    else {
+      var elementParameters = {
+        id: "elementParameters",
+        label: "Element Parameters",
+        groups: elementParametersGroup({ element, bpmnFactory, canvas, elementRegistry, translate })
+      };
 
-    return [scenarioAttributes, scenarioParameters];
+      tabsArr.push(elementParameters)
+
+    }
+    return tabsArr
   };
 }
 

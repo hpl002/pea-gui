a332a58bef92d4bafe127011ccd4499a0cd68721
Herman Stoud Platou <hsplatou@gmail.com>
2021-03-01 20:15:46 +0100

    wip bpsim integration

--- react/src/modeler-configs/provider/props/scenario-props/attribute-factory.js
+++ react/src/modeler-configs/provider/props/scenario-props/attribute-factory.js
@@ -5,6 +5,69 @@ import cmdHelper from "bpmn-js-properties-panel/lib/helper/CmdHelper";
 import entryFactory from "bpmn-js-properties-panel/lib/factory/EntryFactory";
 import { getBusinessObject } from 'bpmn-js/lib/util/ModelUtil';
 import scenarioHelper from '../../../helper/bpsim';
+import lodash from "lodash";
+
+
+const addNewElementParameters = ({ element, moddle, extensionElements }) => {
+    extensionElements.values[0].scenario[0].elementParameters = [
+        moddle.create('bpsim:ElementParameters', {
+            elementRef: element.id
+        })
+    ]
+
+    return {
+        cmd: 'properties-panel.update-businessobject',
+        context: {
+            element: element.businessObject.$parent,
+            businessObject: element.businessObject.$parent,
+            properties: { extensionElements: extensionElements }
+        }
+    }
+}
+
+const capitalize = (s) => {
+    if (typeof s !== 'string') return ''
+    return s.charAt(0).toUpperCase() + s.slice(1)
+}
+
+// get the ElementParameters element 
+// append the correct child to this elememt (e..g TimeParameters)
+const addNewElementParametersChild = ({ element, type, moddle, extensionElements }) => {
+    //her we want to add the TimeParameters element
+    const newElement = moddle.create(`bpsim:${capitalize(type)}`)
+    extensionElements.values[0].scenario[0].elementParameters[0][type] = newElement
+
+    return {
+        cmd: 'properties-panel.update-businessobject',
+        context: {
+            element: element.businessObject.$parent,
+            businessObject: element.businessObject.$parent,
+            properties: { extensionElements: extensionElements }
+        }
+    }
+}
+
+const addNewElementParametersChildChild = ({ element, type, moddle, extensionElements, parent, value = "" }) => {
+    //TransferTime
+    const newElement = moddle.create(`bpsim:${capitalize(type)}`, {
+        resultRequest: [
+            moddle.create('bpsim:ResultRequest')
+        ],
+        parameterValue: [
+            moddle.create('bpsim:ParameterValue', { text: value })
+        ],
+    })
+    extensionElements.values[0].scenario[0].elementParameters[0][parent][0][type] = newElement
+
+    return {
+        cmd: 'properties-panel.update-businessobject',
+        context: {
+            element: element.businessObject.$parent,
+            businessObject: element.businessObject.$parent,
+            properties: { extensionElements: extensionElements }
+        }
+    }
+}
 
 
 const factory = {}
@@ -145,7 +208,7 @@ factory.scenarioParameter.elements = ({ group, element, bpmnFactory, translate, 
 
 factory.elementParameters.attributes = ({ group, element, bpmnFactory, translate, attribute, type = "textField", moddle }) => {
     const identifier = `elementParameters${attribute}`
-    const parameters = getElementParameters(element)
+    var parameters = getElementParameters(element) ? getElementParameters(element) : []
 
     const currentParameter = parameters.filter(x => x.elementRef === element.id)
 
@@ -173,20 +236,71 @@ factory.elementParameters.attributes = ({ group, element, bpmnFactory, translate
 
 factory.elementParameters.elements = ({ group, element, bpmnFactory, translate, type = "textField", moddle, parentElement, childElement }) => {
     const identifier = `elementParameters${childElement}`
-    const parameters = getElementParameters(element)
+    var elementParameters = getElementParameters(element) ? getElementParameters(element) : []
 
-    const currentParameter = parameters.filter(x => x.elementRef === element.id)
 
     var getValue = function () {
+        var currentParameter = elementParameters.filter(x => x.elementRef === element.id)
         return function (element) {
             const response = {};
-            if (currentParameter?.[0]?.[childElement]) response[identifier] = currentParameter[0][childElement]
+            const value = currentParameter?.[0]?.[parentElement]?.[0]?.[childElement]?.[0]?.parameterValue?.[0]?.text
+            if (value) response[identifier] = value
             return response;
         };
     };
 
 
+    const setValue = function (businessObject) {
+        return function (element, values) {
+            const extensionElements = scenarioHelper.getExtensinoElements(element.businessObject.$parent)
 
+            const childOptions = {}
+            // create transferTime element with its accompanying value
+            childOptions[childElement] = [moddle.create(`bpsim:${capitalize(childElement)}`, {
+                resultRequest: [
+                    moddle.create('bpsim:ResultRequest')
+                ],
+                parameterValue: [
+                    moddle.create('bpsim:ParameterValue', { text: values?.[identifier] ? values?.[identifier] : "" })
+                ],
+            })]
+            const options = {}
+            options.elementRef = element.id
+            options[parentElement] = [moddle.create(`bpsim:${capitalize(parentElement)}`, childOptions)]
+
+            // TODO: see if there are any existing entries wit the same id, if so then merge these together
+
+            const scenario = extensionElements.values[0].scenario[0]
+            if (!scenario?.elementParameters) {
+                scenario.elementParameters = []
+                console.log("there are no ElementParameters, initialize new")
+                scenario.elementParameters.push(moddle.create('bpsim:ElementParameters', options))
+            }
+            else {
+                // get index of element with matching elementRef
+                const elementIndex = scenario.elementParameters.findIndex(e => e.elementRef === element.id)
+                if (elementIndex > -1) {
+                    console.log("found existing entry for element with id", element.id)
+                    const newElement = moddle.create('bpsim:ElementParameters', options)
+                    const existingElement = scenario.elementParameters[elementIndex]
+                    scenario.elementParameters[elementIndex] = lodash.merge(existingElement, newElement)
+                }
+                else {
+                    console.log("array exists, but there are no pre-existing elements with matching id")
+                    scenario.elementParameters.push(moddle.create('bpsim:ElementParameters', options))
+                }
+            }
+
+            return {
+                cmd: 'properties-panel.update-businessobject',
+                context: {
+                    element: element.businessObject.$parent,
+                    businessObject: element.businessObject.$parent,
+                    properties: { extensionElements: extensionElements }
+                }
+            }
+        };
+    };
 
     var elementDocuEntry = entryFactory[type](translate, {
         id: identifier,
@@ -195,9 +309,12 @@ factory.elementParameters.elements = ({ group, element, bpmnFactory, translate, 
     });
 
     elementDocuEntry.get = getValue(getBusinessObject(element));
+    elementDocuEntry.set = setValue(getBusinessObject(element));
     group.entries.push(elementDocuEntry);
 
 }
+
+
 
 
 
